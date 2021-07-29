da9f35d1ad6f25663b6c82f638f587e07305c58f
hmorri <84566900+hmorri@users.noreply.github.com>
2021-07-29 03:47:57 -0700

    Add files via upload

--- FindImage.py
+++ FindImage.py
@@ -1,7 +1,9 @@
+from PIL import Image
 import os.path
 import requests
 import shutil
 
+LIMIT = 250
 directory = "Images"
 #Checks for and creates directory for images
 if not os.path.isdir(directory):
@@ -10,8 +12,9 @@ if not os.path.isdir(directory):
    
 #Downloads and saves an image
 
-image_location = "https://live.staticflickr.com/5122/5264886972_3234d62748.jpg"
-file = "test.jpg"
+#image_location = "https://live.staticflickr.com/5122/5264886972_3234d62748.jpg"
+image_location = "https://live.staticflickr.com/2106/2207159142_8206ab6984.jpg"
+file = "Images/test.jpg"
 
 #Opens image
 image = requests.get(image_location, stream = True)
@@ -20,5 +23,22 @@ image = requests.get(image_location, stream = True)
 if image.status_code == 200:
    image.raw.decode_content = True
    with open(file, 'wb') as pic:
-      #saves in the local directory, be sure to move this to the images folder
       shutil.copyfileobj(image.raw, pic)
+
+#adjusts image to standard size
+if os.path.exists(file):
+   with Image.open(file) as pict:
+      width, height = pict.size
+      if width > height:
+         div = float(width)/float(LIMIT)
+         if div > 1.0:
+            newheight = float(height)/float(div)
+            repict = pict.resize((int(LIMIT), int(newheight)))
+            picture = repict.save(file)
+         
+      else:
+         div = float(height)/float(LIMIT)
+         if div > 1.0:
+            newwidth  = float(width)/float(div)
+            repict = pict.resize((int(newwidth), int(LIMIT)))
+            picture = repict.save(file)
